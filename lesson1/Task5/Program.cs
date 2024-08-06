b7734f2d01293ce2cbf3aebdaba824527526be56
VICTORY <iarotskaia.vv@mail.ru>
2024-08-06 14:19:30 +1000

    add lesson1:Task5

---
+++ lesson1/Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано натуральное число N. Вывести на экран числа от 1 до N.
+
+int N = 100;
+int i =1;
+
+while (i < N)
+{
+    // Console.WriteLine(i);
+    Console.Write(i);
+    Console.Write(' ');
+    i = i + 1;
+}

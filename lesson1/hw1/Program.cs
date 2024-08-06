ba1b602425d391912730c2dca9a77e98f4f4acda
VICTORY <iarotskaia.vv@mail.ru>
2024-08-06 13:44:28 +1000

    add lesson1:hw1

---
+++ lesson1/hw1/Program.cs
@@ -0,0 +1,10 @@
+﻿// count (a*b/(c+d)); a, b, c, d - int or long
+
+int a = 5;
+int b = 3;
+long c = 7;
+long d = 8;
+int prod = a * b;
+long sum = c + d;
+long result = prod / sum;
+Console.WriteLine(result);

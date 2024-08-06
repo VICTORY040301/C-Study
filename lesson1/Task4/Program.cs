1c5fb20ef4f3f68d0d39e18076b5aecce982eadc
VICTORY <iarotskaia.vv@mail.ru>
2024-08-06 14:10:08 +1000

    add lesson1:Task4

---
+++ lesson1/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача с 5-ю гирями
+
+int a = 5;
+int b = 6;
+int c = 2;
+int d = 1;
+int e = 5;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);

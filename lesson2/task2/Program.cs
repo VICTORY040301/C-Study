8df577b76fc63ef3050185c7ca86305e750bfe9a
VICTORY <iarotskaia.vv@mail.ru>
2024-08-06 22:39:03 +1000

    add lesson2:task2

---
+++ lesson2/task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Создать массив и заполнить его целыми 
+// числами от 1 до N (N – произвольное 
+// натуральное число). Вывести элементы 
+// массива на экран.
+
+Console.Write("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+int i = 0;
+
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}

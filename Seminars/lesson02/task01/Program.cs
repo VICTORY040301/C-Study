36ad834b3f1d8982db430873a0530f577ded2435
VICTORY <iarotskaia.vv@mail.ru>
2024-08-09 20:22:42 +1000

    add semnars:lesson02:task01

---
+++ Seminars/lesson02/task01/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и удаляет вторую 
+// цифру этого числа.
+// Примеры
+// a = 256 => 26 
+// a = 891 => 81
+
+System.Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int leftDigit = number / 100;
+    int rigthDigit = number % 10;
+    int result = leftDigit * 10 + rigthDigit;
+    System.Console.WriteLine(result);
+}
+else
+{
+    Console.Write("Число не трёхзначное!!!");
+}

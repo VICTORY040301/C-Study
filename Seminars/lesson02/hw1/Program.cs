bb46e9e11fde4d3f623adb9ecf3ec6da1ba55d43
VICTORY <iarotskaia.vv@mail.ru>
2024-08-09 22:15:06 +1000

    add seminars:lesson02:hw1

---
+++ Seminars/lesson02/hw1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно 
+// одновременно 7 и 23
+// a=50 => нет
+// a=7 => нет
+// a=322 => да
+
+System.Console.Write("Write number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 7 == 0 && a % 23 == 0)
+{
+    System.Console.WriteLine("yes");
+}
+else
+{
+    System.Console.WriteLine("no");
+}

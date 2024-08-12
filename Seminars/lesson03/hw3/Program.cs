ad7572b268531adbfa7086a5e572a16b020e69fe
VICTORY <iarotskaia.vv@mail.ru>
2024-08-12 22:41:57 +1000

    add seminars:lesson03:hw3

--- Seminars/lesson03/hw3/Program.cs
+++ Seminars/lesson03/hw3/Program.cs
@@ -3,16 +3,20 @@
 // массив [2.2 0.4 9.11 7.2 78.98] => 78.58
 // массив [1.22 4.5 3.33] => 3.28
 
-double[] numbers = {2.2, 0.4, 9.11, 7.2, 78.98}; 
-double max = numbers[i];
-double min = numbers[i];
+double[] numbers = {1.22, 4.5, 3.33}; 
 
 foreach (var number in numbers)
 {
     System.Console.Write($"{number} ");
 }
+
 int i = 0;
-for (int i = 1; i < numbers.Length - 1; i++)
+double max = numbers[i];
+double min = numbers[i];
+
+// System.Console.Write($"{max}, {min}");
+
+for (i = 0; i < numbers.Length; i++)
 {
     if (numbers[i] > max)
     {
@@ -25,4 +29,5 @@ for (int i = 1; i < numbers.Length - 1; i++)
 }
 
 double diff = max - min;
+string s = string.Format("{0 : 0.00}%", diff);
 System.Console.Write($"=> {diff}");
